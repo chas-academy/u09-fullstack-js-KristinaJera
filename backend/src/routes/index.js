74dcb330d05c31b4a48c3452a86a1ff6dcbaeaa6
KristinaJera <kristina.jera@chasacademy.se>
2024-09-03 11:23:06 +0200

    job routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoute from "./authRoutes.js";
 import userRoute from "./userRoutes.js";
+import companyRoute from "./companiesRoutes.js";
+import jobRoute from "./jobsRoutes.js";
 
 const router = express.Router();
 
@@ -8,5 +10,7 @@ const path = "/api-v1/";
 
 router.use(`$(path)auth`, authRoute); //api-v1/auth/
 router.use(`$(path)users`, userRoute);
+router.use(`$(path)companies`, companyRoute);
+router.use(`$(path)jobs`, jobRoute);
 
 export default router;
