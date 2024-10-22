76168ac7cfa860e96776a911a580db0e4ba0d561
KristinaJera <kristina.jera@chasacademy.se>
2024-10-22 22:03:52 +0200

    pwa installation

--- frontend/jobseeking/vite.config.js
+++ frontend/jobseeking/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './public/manifest.json'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: manifest,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,svg,png}']
+      }
+    })
+  ],
   build: {
     outDir: 'dist', // This is the folder where the production build will be generated
   },
